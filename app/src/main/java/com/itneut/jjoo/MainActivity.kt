cb59dd21d30bbdf6865578db98291d74afaf42f7
Gothman240 <paez.federico96@gmail.com>
2024-11-20 18:54:26 -0300

    organizando navegación

--- app/src/main/java/com/itneut/jjoo/MainActivity.kt
+++ app/src/main/java/com/itneut/jjoo/MainActivity.kt
@@ -7,48 +7,26 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.itneut.jjoo.controller.NavigationController
 import com.itneut.jjoo.repositories.EventRepository
 import com.itneut.jjoo.ui.event.EventAdapter
 import com.itneut.jjoo.ui.event.EventFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navigationController: NavigationController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        // Encuentra el NavController y BottomNavigationView
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
         val navController = navHostFragment.navController
-
-        // navegación
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.menu.findItem(R.id.nav_home).setIcon(R.drawable.ic_home_fill)
-        bottomNavigationView.setOnItemSelectedListener { item ->
-            bottomNavigationView.menu.findItem(R.id.nav_home).setIcon(R.drawable.ic_home)
-            bottomNavigationView.menu.findItem(R.id.nav_medals).setIcon(R.drawable.ic_medal)
-            bottomNavigationView.menu.findItem(R.id.nav_profile).setIcon(R.drawable.ic_profile)
-            when (item.itemId) {
-                R.id.nav_home -> {
-                    item.setIcon(R.drawable.ic_home_fill)
-                    navController.navigate(R.id.eventFragment)
-                    true
-                }
 
-                R.id.nav_medals -> {
-                    item.setIcon(R.drawable.ic_medal_fill)
-                    navController.navigate(R.id.medalFragment)
-                    true
-                }
-
-                R.id.nav_profile -> {
-                    item.setIcon(R.drawable.ic_profile_fill)
-                    navController.navigate(R.id.profileFragment)
-                    true
-                }
-
-                else -> false
-            }
-        }
+        // Configura el controlador de navegación
+        navigationController = NavigationController(navController, bottomNavigationView)
+        navigationController.setupNavigation()
 
     }
 
