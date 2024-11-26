e133441d907f4482bb0732068e8c77b351991c6e
Gothman240 <paez.federico96@gmail.com>
2024-11-25 23:06:56 -0300

    agregando estilos login y perfil

--- app/src/main/java/com/itneut/jjoo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/itneut/jjoo/ui/profile/ProfileFragment.kt
@@ -2,9 +2,11 @@ package com.itneut.jjoo.ui.profile
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.itneut.jjoo.R
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.itneut.jjoo.repositories.UserRepository
 
 class ProfileFragment : Fragment(R.layout.fragment_profile){
@@ -12,19 +14,31 @@ class ProfileFragment : Fragment(R.layout.fragment_profile){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val name = view.findViewById<TextView>(R.id.name)
         val nickname = view.findViewById<TextView>(R.id.nickname)
+        val name = view.findViewById<TextView>(R.id.name)
+        val profilePicture = view.findViewById<TextView>(R.id.profilePicture)
+        val logOut = view.findViewById<LinearLayout>(R.id.LogOut)
 
         val user = UserRepository.loggedInUser
+
+        logOut.setOnClickListener {
+            UserRepository.logout()
+            findNavController().navigate(R.id.action_profileFragment_to_logInFragment)
+        }
 
         if (user != null) {
             val fullName = "${user.name} ${user.surname}"
             name.text = fullName
             nickname.text = user.nickName
+            profilePicture.text = createProfilePicture(fullName)
 
             /*Falta Recycler View aquí*/
 
         }
 
+    }
+
+    fun createProfilePicture(name: String): String{
+        return name.substring(0, 1).uppercase()
     }
 }
