24921dc5c5db061ec5ca69f56f9f06d3d179d776
Gonzalo Nizzi <gnizzi1990@gmail.com>
2024-11-26 00:04:50 -0300

    tercer push

---
+++ app/src/main/java/com/itneut/jjoo/ui/profile/ProfileAdapter.kt
@@ -0,0 +1,52 @@
+package com.itneut.jjoo.ui.profile
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.itneut.jjoo.R
+import com.itneut.jjoo.data.Country
+import com.itneut.jjoo.data.Event
+import com.itneut.jjoo.data.Purchase
+import com.itneut.jjoo.repositories.EventRepository
+import com.itneut.jjoo.repositories.PurchaseRepository
+import com.itneut.jjoo.repositories.UserRepository
+import com.itneut.jjoo.ui.login.LogInFragment
+
+class ProfileAdapter(private val purchases: List<Purchase>) :
+    RecyclerView.Adapter<ProfileAdapter.ProfileHolder>() {
+    class ProfileHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val sportValue: TextView = itemView.findViewById(R.id.tvSportValue)
+        val dateValue: TextView = itemView.findViewById(R.id.tvDateValue)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_profile, parent, false)
+        return ProfileHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: ProfileHolder, position: Int) {
+        val purchase = purchases[position]
+        val userId : Long = purchase.userId
+        val events = EventRepository.get()
+        var sportType: String? = null
+        val eventID: Long = purchase.eventId
+
+        for (event in events) {
+            if (event.id == eventID && userId  == UserRepository.loggedInUser?.id ) {
+                sportType = event.sport.name
+                holder.sportValue.text = sportType
+                holder.dateValue.text = purchase.createdDate
+            }
+        }
+
+
+    }
+
+    override fun getItemCount(): Int = purchases.size
+
+}
--- app/src/main/java/com/itneut/jjoo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/itneut/jjoo/ui/profile/ProfileFragment.kt
@@ -5,12 +5,20 @@ import android.view.View
 import android.widget.TextView
 import com.itneut.jjoo.R
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.itneut.jjoo.repositories.MedalTableRepository
+import com.itneut.jjoo.repositories.PurchaseRepository
 import com.itneut.jjoo.repositories.UserRepository
+import com.itneut.jjoo.ui.medal.MedalTableAdapter
 
 class ProfileFragment : Fragment(R.layout.fragment_profile){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rvProfile)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = ProfileAdapter(PurchaseRepository.get())
 
         val name = view.findViewById<TextView>(R.id.name)
         val nickname = view.findViewById<TextView>(R.id.nickname)
@@ -21,8 +29,6 @@ class ProfileFragment : Fragment(R.layout.fragment_profile){
             val fullName = "${user.name} ${user.surname}"
             name.text = fullName
             nickname.text = user.nickName
-
-            /*Falta Recycler View aquí*/
 
         }
 
